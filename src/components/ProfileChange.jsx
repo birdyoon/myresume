82a0ff9a4d66a5367b597ffe483b4edf61fd2dbd
seyoon <se981106@naver.com>
2024-10-30 01:30:55 +0900

    20241030 프로필 사진첨부기능 삽입

---
+++ src/components/ProfileChange.jsx
@@ -0,0 +1,50 @@
+import DefaultImage from './Image/DefaultImage.png';
+import { useRef, useState } from 'react';
+
+const ProfileChange = () => {
+
+    const [Image, setImage] = useState(DefaultImage);
+    const [File, setFile] = useState('');
+    const fileInput = useRef(null);
+
+    const onChange = (e) => {  
+        if (e.target.files[0]) {
+          setFile(e.target.files[0]);
+        } else {
+          //업로드 취소할 시
+          setImage(DefaultImage);
+          return;
+        }
+
+            //화면에 프로필 사진 표시
+        const reader = new FileReader();  // (5)번 설명
+        reader.onload = () => {
+            if (reader.readyState === 2) {
+                setImage(reader.result);
+            }
+        };
+        reader.readAsDataURL(e.target.files[0]);
+    };
+
+    return(
+        <div className="">
+            <p>프로필변경</p>
+            <div className="Profile">
+                <img src={Image} alt="프로필 사진" className="" />
+                <label className="" htmlFor="input-file">
+                    <span className="">이미지 찾아보기</span>
+                    <input type="file" id="input-file" accept="image/*" className=""
+                    onClick={() => {
+                        fileInput.current.value = null;
+                        fileInput.current.click();
+                      }}
+                      ref={fileInput}
+                      onChange={onChange}/>
+                </label>
+                <button type="button" className=""></button>
+            </div>
+        </div>
+    )
+}
+
+export default ProfileChange;
--- src/pages/PortfollioPage.jsx
+++ src/pages/PortfollioPage.jsx
@@ -1,7 +1,13 @@
+import ProfileChange from "../components/ProfileChange";
+
+
+
+
 const PortfolioPage = () => {
     return (
         <>
             <h2>PortfolioPage</h2>
+            <ProfileChange />
         </>
     )
 }
